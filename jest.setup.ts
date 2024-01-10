8ea77d45f7f80c34c0eba165bc0c9b599968e89f
Oliver Caine <olliecaine@gmail.com>
2024-01-10 23:04:45 +1100

    beforeAll and afterAll

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,16 +1,7 @@
-// import db from "./src/db";
-
-// // enables the fake database for all test files
-// jest.mock("./src/db");
-
-// // run migrations
 // beforeAll(async () => {
-//     await db.migrate.latest();
 // });
 
-// // close connection
 // afterAll(async () => {
-//     await db.destroy();
 // });
 
 export { }
