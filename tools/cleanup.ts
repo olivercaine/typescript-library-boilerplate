ffd1d037725c4c9642bbca8325b7264802c3cf38
Oliver Caine <olliecaine@gmail.com>
2025-03-12 13:14:38 +1100

    Enable importing from root package.json for multi-module repos (#86)

--- tools/cleanup.ts
+++ tools/cleanup.ts
@@ -1,6 +1,15 @@
-import { readFileSync, writeFileSync } from 'fs';
 
-const packageJson = JSON.parse(readFileSync('./dist/package.json').toString());
-delete packageJson.devDependencies;
-delete packageJson.scripts;
-writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2));
+import { readFileSync, writeFileSync } from 'fs'
+
+const packageJson = JSON.parse(readFileSync('./dist/package.json').toString())
+const fieldsToUpdate = ['jsnext:main', 'module', 'types']
+// eslint-disable-next-line pure/pure
+fieldsToUpdate.forEach((field) => {
+  if (packageJson[field] && packageJson[field].startsWith('dist/')) {
+    packageJson[field] = packageJson[field].replace('dist/', '')
+  }
+})
+delete packageJson['devDependencies']
+delete packageJson['scripts']
+delete packageJson['husky']
+writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2))
