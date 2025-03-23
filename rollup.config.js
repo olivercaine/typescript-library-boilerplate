3f17ed2676dcf6744739787333b07772ce183533
Oliver Caine <olliecaine@gmail.com>
2025-03-24 08:57:50 +1100

    Test that build npm module works in node (#101)

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,19 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/typescript-library-boilerplate.bundle.js',
-    format: 'umd',
-    name: 'typescript-library-boilerplate',
-    sourcemap: true
-  },
+  output: [
+    {
+      file: 'dist/typescript-library-boilerplate.bundle.js',
+      format: 'umd',
+      name: 'typescript-library-boilerplate',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
   plugins: [
     nodeResolve(),
     commonjs(),
